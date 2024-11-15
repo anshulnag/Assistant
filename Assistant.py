ae60fe8b2ef81419196f145bc55afb69356cc3e2
RimerMcquvue <95558207+anshulnag@users.noreply.github.com>
2024-11-15 20:46:06 +0530

    update

--- Assistant.py
+++ Assistant.py
@@ -12,8 +12,7 @@ from PIL import Image
 from dotenv import load_dotenv
 
 load_dotenv()
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 def extract_pdf_text(pdf_docs):
     text = ""
